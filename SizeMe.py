9ffa4031b98bf0395f3bafffc0c42002c6dc9981
Aditya Rayapudi <arayapudi2006@gmail.com>
2025-03-01 21:06:03 -0500

    Added Basic Demo Page

--- SizeMe.py
+++ SizeMe.py
@@ -4,7 +4,7 @@ import PoseModule as pm
 
 cap = cv2.VideoCapture(0)
 pTime = 0
-fullTime = 100000000000000
+fullTime = float('inf')
 startCountdown = False
 newTime = 0
 detector = pm.PoseDetector()
@@ -20,6 +20,7 @@ while True:
     if startCountdown:
         newTime = time.time()
         cv2.putText(img, str(int(10-(newTime-startTime))), (250, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
     if cv2.waitKey(1) & 0xFF == ord(' '):
         if buttonPress == 0:
             startTime = time.time()
@@ -27,7 +28,7 @@ while True:
         startCountdown = True
         fullTime = cTime + 10
         newTime = startTime
-        time.process_time()
+
     if cTime >= fullTime:
         break
     
